bb673ba8e996379a02acb91ed6f3871f831d1407
Ahmed Mokaddem <ahm.moka@gmail.com>
2022-11-01 18:19:46 +0100

    correction du code

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        system.out.print("Mon premier piepline java");
+        System.out.print("Mon premier piepline java");
     }
 
 
